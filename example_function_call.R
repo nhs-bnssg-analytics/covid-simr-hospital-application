1fdd60f0b926704d06c09f30a18f65dc2acb7fbc
richardwoodgb <57181505+richardwoodgb@users.noreply.github.com>
2020-05-02 11:48:25 +0100

    Update example_function_call.R

--- example_function_call.R
+++ example_function_call.R
@@ -3,42 +3,48 @@ setwd("...")
 
 source("covid_simr-gamma.R")
 
-cases_do_nothing<-read.csv("baseline-do-nothing.csv")
-cases_current_strategy<-read.csv("baseline-current-strategy.csv")
-cases_current_strategy_flattened<-read.csv("baseline-current-strategy-flattened.csv")
+cases_no_isolation<-read.csv("no-isolation.csv")
+cases_isolation<-read.csv("isolation.csv")
+cases_isolation_flattened<-read.csv("isolation-flattened.csv")
 
 nreps=1000
 
-# scenario1: baseline: do nothing example
-covid_simr("scenario1",cases=cases_do_nothing,galpha=8,gbeta=1,cap=45,pfat=0.99,nreps=nreps)
-
-# scenario2: baseline: current strategy example
-covid_simr("scenario2",cases=cases_current_strategy,galpha=8,gbeta=1,cap=45,pfat=0.99,nreps=nreps)
-
-# scenario3: example mitigation 1 - increase capacity to 76
-covid_simr("scenario3",cases=cases_current_strategy,galpha=8,gbeta=1,cap=76,pfat=0.99,nreps=nreps)
-
-# scenario4: example mitigation 1 - increase capacity to 100
-covid_simr("scenario4",cases=cases_current_strategy,galpha=8,gbeta=1,cap=100,pfat=0.99,nreps=nreps)
-
-# scenario5: example mitigation 2 - reduce LOS by 10%
-covid_simr("scenario5",cases=cases_current_strategy,galpha=6,gbeta=1,cap=45,pfat=0.99,nreps=nreps)
-
-# scenario6: example mitigation 2 - flatten curve (stretch 50%)
-covid_simr("scenario6",cases=cases_current_strategy_flattened,galpha=8,gbeta=1,cap=45,pfat=0.99,nreps=nreps)
-
-# scenario7: example mitigation 2 - flatten curve (stretch 50%) + reduce LOS + increase capacity
-covid_simr("scenario7",cases=cases_current_strategy_flattened,galpha=6,gbeta=1,cap=100,pfat=0.99,nreps=nreps)
-
-# scenario99: capacity unconstrained
-covid_simr("scenario99",cases=cases_current_strategy,galpha=8,gbeta=1,cap=10000,pfat=0.99,nreps=nreps)
+galpha1<-1.660265
+gbeta1<-0.205719
+galpha75<-1.659735
+gbeta75<-0.274203
+galpha125<-1.660882
+gbeta125<-0.164646
 
 
+# scenario1: do nothing
+covid_simr("scenario1",cases=cases_no_isolation,galpha=galpha1,gbeta=gbeta1,cap=45,pfat=0.99,nreps=nreps)
+
+# scenario2: isolation strategy
+covid_simr("scenario2",cases=cases_isolation,galpha=galpha1,gbeta=gbeta1,cap=45,pfat=0.99,nreps=nreps)
+
+# scenario3: increase capacity to 76
+covid_simr("scenario3",cases=cases_isolation,galpha=galpha1,gbeta=gbeta1,cap=76,pfat=0.99,nreps=nreps)
+
+# scenario4: increase capacity to 100
+covid_simr("scenario4",cases=cases_isolation,galpha=galpha1,gbeta=gbeta1,cap=100,pfat=0.99,nreps=nreps)
+
+# scenario5: example mitigation - reduce LOS by 25%
+covid_simr("scenario5",cases=cases_isolation,galpha=galpha75,gbeta=gbeta75,cap=45,pfat=0.99,nreps=nreps)
+
+# scenario6: increase LOS by 25%
+covid_simr("scenario6",cases=cases_isolation,galpha=galpha125,gbeta=gbeta125,cap=45,pfat=0.99,nreps=nreps)
+
+# scenario7: flatten curve (stretch 50%)
+covid_simr("scenario7",cases=cases_isolation_flattened,galpha=galpha1,gbeta=gbeta1,cap=45,pfat=0.99,nreps=nreps)
+
+# scenario8: flatten curve (stretch 50%) + increase capacity to 100 + reduce LOS by 25%
+covid_simr("scenario8",cases=cases_isolation_flattened,galpha=galpha75,gbeta=gbeta75,cap=100,pfat=0.99,nreps=nreps)
 
 
 
-
-
+# scenario 99: no capacity constraint
+covid_simr("scenario99",cases=cases_isolation,galpha=galpha1,gbeta=gbeta1,cap=10000,pfat=0.99,nreps=nreps)
 
 
 
